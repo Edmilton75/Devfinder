10df967cb33dcacf4c98d1076a94d1ae47ea47cc
edmilton <edmilton7500@gmail.com>
2023-02-09 16:26:07 -0300

    fix style e função criar card

--- script.js
+++ script.js
@@ -1,122 +1,132 @@
 const form = document.querySelector("form");
-const btn = document.querySelector(".btn-search");
-const input = document.querySelector(".input-search");
-let valor = "";
+const searchBtn = document.querySelector(".btn-search");
+const searchInput = document.querySelector(".input-search");
 
-async function fetchUserGithub(event) {
-  event.preventDefault();
-  valor = input.value;
-  const userResponse = await fetch(`https://api.github.com/users/${valor}`);
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  cardGereration(searchInput.value);
+});
+
+async function cardGereration(userJson1) {
+  const userResponse = await fetch(`https://api.github.com/users/${userJson1}`);
+
+  console.log(userResponse.status);
   const userJson = await userResponse.json();
+  if (userResponse.status === 200) {
+    const container = document.querySelector(".container");
 
-  const container = document.querySelector(".container");
+    const section = document.querySelector("section");
 
-  const section = document.querySelector("section");
+    if (section) {
+      container.removeChild(section);
+    }
 
-  const card = document.querySelector(".card-container");
+    const sectionUser = document.createElement("section");
 
-  if (section) {
-    container.removeChild(card);
-  }
+    sectionUser.classList.add("card-container");
 
-  const sectionUser = document.createElement("section");
+    container.appendChild(sectionUser);
 
-  sectionUser.classList.add("card-container");
+    const meses = [
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez",
+    ];
 
-  container.appendChild(sectionUser);
+    let data = new Date(userJson.created_at);
+    let dataFormatada = `
+      ${data.getDate()} ${meses[data.getMonth()]} ${data.getFullYear()}`;
 
-  // formatar data com mes
-  const meses = [
-    "Jan",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Mai",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Set",
-    "Out",
-    "Nov",
-    "Dez",
-  ];
-
-  let data = new Date(userJson.created_at);
-  let dataFormatada = `
-    ${data.getDate()} ${meses[data.getMonth()]} ${data.getFullYear()}`;
-
-  sectionUser.innerHTML = `
-  <div class="perfil">
-          <img class="avatar" src="${userJson.avatar_url}" alt="" href="" />
+    sectionUser.innerHTML = `
+        <div class="perfil">
+          <img
+            class="avatar"
+            src="${userJson.avatar_url}"
+            alt="Avatar usuario"
+          />
+        </div>
+        <div class="user-info">
           <div class="info">
             <div class="name-sub">
               <h1 class="perfil-name">${userJson.name}</h1>
-              <p class="perfil-sub">@${userJson.login}</p>
-              <p class="perfil-bio">${userJson.bio || "Not found"}</p>
+              <p class="perfil-sub">${userJson.login}</p>
+              <p class="perfil-bio">${userJson.login}</p>
             </div>
             <div class="perfil-date">
-              <p>joined ${dataFormatada}</p>
+              <p>Joined ${dataFormatada}</p>
+            </div>
+          </div>
+          <div class="card-status">
+            <div class="profile-status-container">
+              <div class="repos">
+                <p class="repos-sub">repos</p>
+                <p class="repos-number">${userJson.public_repos}</p>
+              </div>
+              <div class="followers">
+                <p class="repos-sub">followers</p>
+                <p class="repos-number">${userJson.followers}</p>
+              </div>
+              <div class="following">
+                <p class="repos-sub">following</p>
+                <p class="repos-number">${userJson.following}</p>
+              </div>
+            </div>
+          </div>
+          <div class="social-network">
+            <div class="container-location-github">
+              <div class="cont-loc-git ${userJson.location ? "" : "disabled"}">
+                <img src="./assets/mapPin.svg" alt="icone de localização" />
+                <p class="user-location">${
+                  userJson.location || "Not available"
+                }</p>
+              </div>
+
+              <div class="cont-loc-git ${userJson.html_url ? "" : "disabled"}">
+                <a href=${userJson.html_url}>
+                  <img src="./assets/link.svg" alt="icone do github" />
+                  <p class="user-github">${
+                    userJson.html_url || "Not available"
+                  }</p>
+                </a>
+              </div>
+            </div>
+
+            <div class="container-twitter-building">
+              <div class="cont-twit-build ${
+                userJson.twitter_username ? "" : "disabled"
+              }">
+                <a href="https://${userJson.twitter_username}">
+                  <img src="./assets/twitterlogo.svg" alt="icone do twitter" />
+                  <p class="user-twitter">${
+                    userJson.twitter_username || "Not available"
+                  }</p>
+                </a>
+              </div>
+
+              <div class="cont-twit-build ${userJson.blog ? "" : "disabled"}">
+                <a href="http://www.${userJson.blog}">
+                  <img src="./assets/buildings.svg" alt="icone do portifolio" />
+                  <p class="user-portifolio">${
+                    userJson.blog || "Not available"
+                  }</p>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div class="card-status">
-          <div class="profile-status-container">
-            <div class="repos">
-              <p class="repos-sub ">repos</p>
-              <p class="repos-number">${userJson.public_repos}</p>
-            </div>
-            <div class="followers">
-              <p class="repos-sub">followers</p>
-              <p class="repos-number">${userJson.followers}</p>
-            </div>
-            <div class="following">
-              <p class="repos-sub">following</p>
-              <p class="repos-number">${userJson.following}</p>
-            </div>
-          </div>
-        </div>
-        <div class="social-network">
-          <div class="container-location-github">
-            <div class="cont-loc-git ${userJson.location ? "" : "disabled"}">
-            
-              <img src="./assets/mapPin.svg" alt="icone de localização" />
-              <p class="user-location">${
-                userJson.location || "Not available"
-              }</p>
-            </div>
-
-            <div class="cont-loc-git ${userJson.html_url ? "" : "disabled"}">
-            <a href=${userJson.html_url}>
-          
-            <img src="./assets/link.svg" alt="icone do github" />
-            <p class="user-github">${userJson.html_url || "Not available"}</p>
-        </a>
-            </div>
-          </div>
-
-          <div class="container-twitter-building">
-            <div class="cont-twit-build ${
-              userJson.twitter_username ? "" : "disabled"
-            }">
-            <a href="https://${userJson.twitter_username}">
-          
-            <img src="./assets/twitterlogo.svg" alt="icone do twitter" />
-            <p class="user-twitter">${
-              userJson.twitter_username || "Not available"
-            }</p>
-            </a>
-            </div>
-
-            <div class="cont-twit-build ${userJson.blog ? "" : "disabled"}">
-            <a href=http://www.${userJson.blog}>
-            
-            <img src="./assets/buildings.svg" alt="icone do portifolio" />
-            <p class="user-portifolio">${userJson.blog || "Not available"}</p>
-            </a>
-            </div>
-          </div>
-        </div>
-  `;
+    `;
+  } else if (searchInput.value === "") {
+    alert("Usuario não preenchido");
+  } else {
+    alert("Usuario não encontrado");
+  }
 }
-
-btn.addEventListener("click", fetchUserGithub);
